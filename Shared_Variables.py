4ed7b3c35d38139ba33f620676ca6604a2691a47
Nainho1703 <110940303+Nainho1703@users.noreply.github.com>
2023-12-13 00:17:01 +0100

    Update Shared_Variables.py

--- Shared_Variables.py
+++ Shared_Variables.py
@@ -4,6 +4,8 @@ global max_id_user_model
 max_id_user_model = 610
 global loggedIn
 loggedIn = False
+global W_matrix
+W_matrix=[]
 global connection_string
 connection_string = 'Driver={ODBC Driver 18 for SQL Server};Server=tcp:agiledsproject.database.windows.net,1433;Database=MovieRatings;Uid=ProjectDataBase;Pwd=Agile2023;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;'
 
